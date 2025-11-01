a2151816784339457f1de89b4a79a66233e92020
aleya nazifa <132155116+AleyaNazifa@users.noreply.github.com>
2025-11-01 15:17:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="HFMD Malaysia Dashboard", page_icon="🦠")
 
 home = st.Page("home.py", title="Temporal & Seasonal Trend", icon=":material/insights:", default=True)
-visualise = st.Page("hfmd_visualisation.py", title="Weather Correlation", icon=":material/bar_chart:")
+visual = st.Page("hfmd_visualisation.py", title="Weather Correlation", icon=":material/bar_chart:")
 regional = st.Page("regional_comparison.py", title="Regional Comparison", icon=":material/map:")
 
-pg = st.navigation({"Menu": [home, visualise, regional]})
+pg = st.navigation({"Menu": [home, visual, regional]})
 pg.run()
